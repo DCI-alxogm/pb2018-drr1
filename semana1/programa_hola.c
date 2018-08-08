b798252c6d199c9efb4bb55b869ff1de6b9785f9
Rosales Rosales Diana <drr1@github.com>
2018-08-08 09:44:00 -0500

    Cree un nuevo programa

---
+++ semana1/programa_hola.c
@@ -0,0 +1,17 @@
+
+
+/*Creado el 08 de Agosto de 2018 por Diana Rosales*/
+#include<stdio.h>
+int main()
+{
+	printf("Hola Diana\n");
+	printf("Este es el primer programa del curso\n");
+}
+/*Termine de escribir el programa*/
+
+
+
+
+/* Esto es un comentario. Me sirve para escribir informacion relevante del programa que estoy haciendo. Creado el 06/08/2018 por Rosales Rosales Diana
+*/
+
