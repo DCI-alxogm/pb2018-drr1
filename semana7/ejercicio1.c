00bffe714c789eb7f3bdb71d34917667bee86b14
Rosales Rosales Diana <rosalesrd2018@dci25-175.ugto.mx>
2018-09-19 09:06:16 -0500

    Modifique el archivo, completo

--- semana7/ejercicio1.c
+++ semana7/ejercicio1.c
@@ -1,36 +1,37 @@
 //este programa lo creo diana rosales el 17 de sep 2018
-#include<stdio.h>
-#include<math.h>
+#include<stdio.h> //incluyo librerias necesarias
+#include<math.h> //incluyo libreria de matematicas para realizar operaciones
 int main(){
 
 
-	FILE *fp;
-	FILE *fp2;	
-	int P, Xmin, Xmax, N,   h, k=10,  x; 
-	float	 fx, deltax, integral;
+	FILE *fp; //doy de alta variable para abrir otro archivo
+	FILE *fp2; //doy de alta para abrir un segundo archivo	
+	int     k=10; //doy de alta variable entera
+	float	h, P, Xmin, Xmax, N, fx, deltax, x, integral; //doy de alta variables flotantes, por si son decimales
 
-	fp=fopen("entrada1.txt","r");
-	fscanf(fp,"%i %i %i %i",&P, &Xmin, &Xmax, &N);
-	fclose(fp);
-	fp2=fopen("resumen1.txt","w");
+	fp=fopen("entrada1.txt","r"); //abro en read el archivo del que sacaré los datos
+	fp2=fopen("resumen1.txt","w"); //abro el archivo en el que imprimire los resultados
 
-	deltax=(Xmax-Xmin)/N;
+	fscanf(fp,"%f %f %f %f",&P,&Xmin,&Xmax,&N); //escaneo desde el archivo los datos
+	printf("recuerda que tus resultados se imprimiran en resumen1.txt\n"); //imprimo para que el usuario revise 
 
-	for(x=Xmin; x<=Xmax; x+=deltax){
-		    fx=pow(x,P);	
+	deltax=(Xmax-Xmin)/N; //delta es el cambio que llevara entre cada valor de x 
+
+	for(x=Xmin; x<=Xmax; x+=deltax){ //para que empiece de el minimo valor hasta al maximo sumando el delta 													calculado
+		    fx=pow(x,P); //para sacar la funcion de X, que es X a la potencia leida 	
 
 
-	h=(x-Xmin)/k;
-		integral = (pow(Xmin,P) + pow(x,P))/2;
-		for(int i=1 ; i<k ; i++){
-			integral += pow((Xmin+i*h), P);
-		}
-		integral*=h;
+	h=(x-Xmin)/k; //la formula para sacar H de acuerdo a K dado en este archivo
+		integral = (pow(Xmin,P) + pow(x,P))/2; //la primera parte de la integral en el que sumo las dos 							potencias de Xinicial y X y la divido entre dos
+		for(int i=1 ; i<k ; i++){ //empiezo ciclo For para la Sumatoria
+			integral += pow((Xmin+i*h), P); //a la suma anterior, se le suma lo de sumatoria por XMin*h 								calculada
+				integral= integral*h; //al resultado obtenido anteriormente, se le multiplica por h, 								segun la forumla
+					}
 		
-		fprintf(fp2, "del numero %i la funcion es %f y la integral %f \n ", x, fx, integral);
-	}
-	
-	fclose(fp2);
+	fprintf(fp2, "del numero %f la funcion es %f y la integral %f \n ", x, fx, integral); //imprimo en el segundo 													archivo los resultados 
+					}
+	fclose(fp); //cierro de donde lei
+	fclose(fp2); //cierro en donde imprimi
 
 	
 }
