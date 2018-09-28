ca175667a8243098965c907f801fa16acf27bda6
Rosales Rosales Diana <drr1@github.com>
2018-09-28 13:42:53 -0500

    aCTUALICE

--- Projecto1/programaplanetas.c
+++ Projecto1/programaplanetas.c
@@ -1,182 +1,72 @@
-//Este proyecto fue creado por Diana Rosales
-//La fecha de entrega es 1 de Octubre 2018
-#include<stdio.h>
-#include<math.h>
-int main() {
-//9 planetas
 
-	FILE *fp;
-	double xo, yo, zo, vxo, vyo, vzo, xn, yn, zn, vxn, vyn, vzn, r;
-	float h=0.01;
-	int tiempo, n, numplan, to=0, t;
+//este programa fue creado por diana rosales para entregar el 1 de octubre
+
+//incluimos las liberarias necesarias
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	
+    FILE *fp; //dar de alta el archivo del cual se leeran
+   FILE *fp2; //para los archivos en los cuales se imprimiran
+	fp = fopen("resumen1.txt", "r"); //se abre el del cual se leera que ya existe
+
+	//declaramos las variables necesarias para el programa
+	
+	int N=9, count=0; //los planetas que son (9) y un contador desde 0
+	char planeta[20]; //para leer el nombre del planeta
+	double  G, xo, yo, zo, vx, vy, vz, h=.001, r; //posiciones, velocidades y otras cantidades necesarias para las operaciones
+
+	//calculamos la constante de gravitacion, que esta dada por 4 pi al cuadrado
+	G=4*pow(3.1416,2);
+
 	
 
-	fp=fopen("resumen1.txt","r");
-	
-	for(n=1;n<10;n++){
-		fscanf(fp,"%i %lf %lf %lf %lf %lf %lf %i \n",&tiempo,&xo,&yo,&zo,&vxo,&vyo,&vzo,&numplan);
-		fclose(fp);
-		 			
-	
-				if(numplan==1){
-				
-						for(t=to;t<=1;t+=h){
-							xn=xo+(vxo*t);
-							yn=yo+(vyo*t);
-							zn=zo+(vzo*t);
-							r=sqrt(pow(xn,2)*pow(yn,2)*pow(zn,2));
-							vxn=vxo-(t*(((4/365)*xo)/pow(r,3)));
-							vyn=vxo-(t*(((4/365)*yo)/pow(r,3)));
-							vzn=vxo-(t*(((4/365)*zo)/pow(r,3)));	
+    	//se abre ciclo for para hacerlo N veces (9 planetas)
+	for(int i=0 ; i<N ; i++){
+		
+		//se lee el nombre, posiciones y velocidades
+		fscanf(fp, "%s %lf %lf %lf %lf %lf %lf \n", &planeta[20], &xo, &yo, &zo, &vx, &vy, &vz);
+		
 
-					fp=fopen("mercurio.txt","w");
-					fprintf(fp,"%lf %lf %lf %lf %lf %lf",xn,yn,zn,vxn,vyn,vzn);
-					fclose(fp);
-								  }
+       		//se multiplica por 365 para sacar en un año, porque estaba en dia
+		vx*=365;
+		vy*=365;
+		vz*=365;
 
-						}
+        	
+        	fp2=fopen(planeta,"w"); //para crear el archivo con cada nombre de planeta
 
-				if(numplan==2){
-				
-						for(t=to;t<=1;t+=h){
-							xn=xo+(vxo*t);
-							yn=yo+(vyo*t);
-							zn=zo+(vzo*t);
-							r=sqrt(pow(xn,2)*pow(yn,2)*pow(zn,2));
-							vxn=vxo-(t*(((4/365)*xo)/pow(r,3)));
-							vyn=vxo-(t*(((4/365)*yo)/pow(r,3)));
-							vzn=vxo-(t*(((4/365)*zo)/pow(r,3)));	
 
-					fp=fopen("venus.txt","w");
-					fprintf(fp,"%lf %lf %lf %lf %lf %lf",xn,yn,zn,vxn,vyn,vzn);
-					fclose(fp);
-								  }
+        	//se hace un ciclo para hacer las operaciones 
+        	for(i=0 ; i<=1 ; i+=h){ //empieza de tiempo 0, maximo 1 año y va de la cte h en h
 
-						}
+           		//solo se imprimira uno de cada diez puntos
+            		if(count%10==0){ //el contador que dimos de alta para empezar en 0
+                   		//se imprime las posiciones y velocidades 
+                    		fprintf(fp2, "\n  %lf, %lf, %lf, %lf , %lf , %lf", vx, vy, vz, xo, yo, zo);
+           	 	}
+            		count++;
 
-				if(numplan==3){
-				
-						for(t=to;t<=1;t+=h){
-							xn=xo+(vxo*t);
-							yn=yo+(vyo*t);
-							zn=zo+(vzo*t);
-							r=sqrt(pow(xn,2)*pow(yn,2)*pow(zn,2));
-							vxn=vxo-(t*(((4/365)*xo)/pow(r,3)));
-							vyn=vxo-(t*(((4/365)*yo)/pow(r,3)));
-							vzn=vxo-(t*(((4/365)*zo)/pow(r,3)));	
+                	//pero como va cambiando, se van sumando las nuevas distancias en x y z
+               		xo+=vx*h;
+                	yo+=vy*h;
+                	zo+=vz*h;
 
-					fp=fopen("tierra.txt","w");
-					fprintf(fp,"%lf %lf %lf %lf %lf %lf",xn,yn,zn,vxn,vyn,vzn);
-					fclose(fp);
-								  }
+                	//se calcula la distancia para el radio
+                	r = sqrt((xo*xo) + (yo*yo) + (zo*zo));
 
-						}
-				if(numplan==4){
-				
-						for(t=to;t<=1;t+=h){
-							xn=xo+(vxo*t);
-							yn=yo+(vyo*t);
-							zn=zo+(vzo*t);
-							r=sqrt(pow(xn,2)*pow(yn,2)*pow(zn,2));
-							vxn=vxo-(t*(((4/365)*xo)/pow(r,3)));
-							vyn=vxo-(t*(((4/365)*yo)/pow(r,3)));
-							vzn=vxo-(t*(((4/365)*zo)/pow(r,3)));	
+                	//se calculan las nuevas velocidades
+                	vx-=h*((G*xo)/pow(r, 3));
+                	vy-=h*((G*yo)/pow(r, 3));
+                	vz-=h*((G*zo)/pow(r, 3));
+        	}
 
-					fp=fopen("marte.txt","w");
-					fprintf(fp,"%lf %lf %lf %lf %lf %lf",xn,yn,zn,vxn,vyn,vzn);
-					fclose(fp);
-								  }
-
-						}
-			if(numplan==5){
-				
-						for(t=to;t<=1;t+=h){
-							xn=xo+(vxo*t);
-							yn=yo+(vyo*t);
-							zn=zo+(vzo*t);
-							r=sqrt(pow(xn,2)*pow(yn,2)*pow(zn,2));
-							vxn=vxo-(t*(((4/365)*xo)/pow(r,3)));
-							vyn=vxo-(t*(((4/365)*yo)/pow(r,3)));
-							vzn=vxo-(t*(((4/365)*zo)/pow(r,3)));	
-
-					fp=fopen("jupiter.txt","w");
-					fprintf(fp,"%lf %lf %lf %lf %lf %lf",xn,yn,zn,vxn,vyn,vzn);
-					fclose(fp);
-								  }
-
-						}
-
-				if(numplan==6){
-				
-						for(t=to;t<=1;t+=h){
-							xn=xo+(vxo*t);
-							yn=yo+(vyo*t);
-							zn=zo+(vzo*t);
-							r=sqrt(pow(xn,2)*pow(yn,2)*pow(zn,2));
-							vxn=vxo-(t*(((4/365)*xo)/pow(r,3)));
-							vyn=vxo-(t*(((4/365)*yo)/pow(r,3)));
-							vzn=vxo-(t*(((4/365)*zo)/pow(r,3)));	
-
-					fp=fopen("saturno.txt","w");
-					fprintf(fp,"%lf %lf %lf %lf %lf %lf",xn,yn,zn,vxn,vyn,vzn);
-					fclose(fp);
-								  }
-
-						}
-
-				if(numplan==7){
-				
-						for(t=to;t<=1;t+=h){
-							xn=xo+(vxo*t);
-							yn=yo+(vyo*t);
-							zn=zo+(vzo*t);
-							r=sqrt(pow(xn,2)*pow(yn,2)*pow(zn,2));
-							vxn=vxo-(t*(((4/365)*xo)/pow(r,3)));
-							vyn=vxo-(t*(((4/365)*yo)/pow(r,3)));
-							vzn=vxo-(t*(((4/365)*zo)/pow(r,3)));	
-
-					fp=fopen("urano.txt","w");
-					fprintf(fp,"%lf %lf %lf %lf %lf %lf",xn,yn,zn,vxn,vyn,vzn);
-					fclose(fp);
-								  }
-
-						}
-
-				if(numplan==8){
-				
-						for(t=to;t<=1;t+=h){
-							xn=xo+(vxo*t);
-							yn=yo+(vyo*t);
-							zn=zo+(vzo*t);
-							r=sqrt(pow(xn,2)*pow(yn,2)*pow(zn,2));
-							vxn=vxo-(t*(((4/365)*xo)/pow(r,3)));
-							vyn=vxo-(t*(((4/365)*yo)/pow(r,3)));
-							vzn=vxo-(t*(((4/365)*zo)/pow(r,3)));	
-
-					fp=fopen("neptuno.txt","w");
-					fprintf(fp,"%lf %lf %lf %lf %lf %lf",xn,yn,zn,vxn,vyn,vzn);
-					fclose(fp);
-								  }
-
-						}
-
-				if(numplan==9){
-				
-						for(t=to;t<=1;t+=h){
-							xn=xo+(vxo*t);
-							yn=yo+(vyo*t);
-							zn=zo+(vzo*t);
-							r=sqrt(pow(xn,2)*pow(yn,2)*pow(zn,2));
-							vxn=vxo-(t*(((4/365)*xo)/pow(r,3)));
-							vyn=vxo-(t*(((4/365)*yo)/pow(r,3)));
-							vzn=vxo-(t*(((4/365)*zo)/pow(r,3)));	
-
-					fp=fopen("pluton.txt","w");
-					fprintf(fp,"%lf %lf %lf %lf %lf %lf",xn,yn,zn,vxn,vyn,vzn);
-					fclose(fp);
-								  }
-
-						}
-			}
-
+        	//cerramos el archivo del planeta 
+		fclose(fp2);
 	}
+	//cerramos el archivo de lectura
+	fclose(fp);
+
+	return 0;
+}
