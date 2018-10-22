832730d59fd78486b90e23b63310a81c8fc209c6
Rosales Rosales Diana <rosalesrd2018@dci25-171.ugto.mx>
2018-10-22 09:46:30 -0500

    CTUALICE

---
+++ semana11/codigo.c
@@ -0,0 +1,22 @@
+//este proyecto fue creado por diana rosales 
+
+#include<math.h>
+#include<stdio.h>
+
+int main(){
+
+float a,b,c,d,N,n,x[n],y[n], epsilon=.0001;
+FILE *fp;
+FILE *fp2;
+
+fp=fopen("datosiniciales.txt","r");
+fscanf(fp,"%f %f %f %f %f",&a,&b,&c,&d,&N);
+fclose(fp);
+
+n=sqrt(N);
+
+Cuadro=x[n],y[n];
+
+
+}
+
